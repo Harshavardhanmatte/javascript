77c8ec5e681de49f409da0fb499d0331d1d47ec9
Harshavardhanmatte <matteharshavardhan@gmail.com>
2024-06-25 14:23:10 +0530

    first commit

---
+++ javascript/add.js
@@ -0,0 +1,12 @@
+function myfunction(x, y) {
+    var result = x + y;
+    console.log(result);
+  }
+  
+  //console.log('hai');
+  myfunction(20, 30); //20 + 30
+  
+  myfunction(100, 200); // 100 +200
+  
+  myfunction(50, 50); // 50 + 50
+  myfunction(1, 2);
